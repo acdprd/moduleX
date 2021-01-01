e541901cc1ca265f27ab6ed6acd4537264056732
acdprd <maksis9@ya.ru>
2021-01-01 05:10:20 +0300

    [] dk

--- adapterandviews/src/main/java/com/acdprd/adapterandviews/adapter/rv/holder/BaseItemHolder.kt
+++ adapterandviews/src/main/java/com/acdprd/adapterandviews/adapter/rv/holder/BaseItemHolder.kt
@@ -3,7 +3,7 @@ package com.acdprd.adapterandviews.adapter.rv.holder
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-abstract class BaseItemHolder<V : View,M:Any>(var baseItemView: V) :
+abstract class BaseItemHolder<V : View, M : Any>(var baseItemView: V) :
     RecyclerView.ViewHolder(baseItemView) {
 
     abstract fun bindItem(model: M)

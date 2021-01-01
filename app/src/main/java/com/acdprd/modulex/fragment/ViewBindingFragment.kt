a30435e70b6cbdd6a3231b42e80d4f4d037e9daa
acdprd <maksis9@ya.ru>
2021-01-01 05:14:04 +0300

    [*] ViewBindingFragment

--- app/src/main/java/com/acdprd/modulex/fragment/ViewBindingFragment.kt
+++ app/src/main/java/com/acdprd/modulex/fragment/ViewBindingFragment.kt
@@ -1,31 +1,21 @@
 package com.acdprd.modulex.fragment
 
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.ViewGroup
 import android.widget.Toast
 import com.acdprd.modulex.R
 import com.acdprd.modulex.databinding.FragmentVbBinding
 import com.acdprd.modulex.fragment.basevb.BaseVbFragment
 import com.acdprd.modulex.fragment.basevb.CreateMethod
-import com.acdprd.modulex.fragment.basevb.viewBinding
+import com.acdprd.modulex.fragment.basevb.viewBindingDelegate
 
 class ViewBindingFragment : BaseVbFragment<FragmentVbBinding>() {
 
+    override val binding: FragmentVbBinding by viewBindingDelegate()
     override fun getLayoutRes(): Int = R.layout.fragment_vb
-    override fun initBinding(inflater: LayoutInflater, v: ViewGroup?) {
-//        super.initBinding(inflater, v)
-        binding = FragmentVbBinding.inflate(inflater, v, false)
-    }
 
     override fun onCreateView() {
 
         binding.tvText.setOnClickListener {
             Toast.makeText(binding.tvText.context, ":TOAST:", Toast.LENGTH_SHORT).show()
         }
-
-        val a: FragmentVbBinding = viewBinding(CreateMethod.BIND)
-        Log.w("AAAA",a::class.java.simpleName)
     }
-
 }

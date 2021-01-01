b2b40e25dbd8bc68e377c5b16701c3b8e8b2d615
acdprd <maksis9@ya.ru>
2021-01-01 05:09:32 +0300

    [*] const

--- app/src/main/java/com/acdprd/modulex/Const.kt
+++ app/src/main/java/com/acdprd/modulex/Const.kt
@@ -7,7 +7,9 @@ object Const {
     enum class ViewType : IViewType{
         UNDEFINED,
         SOME_TYPE_1,
-        SOME_TYPE_2,;
+        SOME_TYPE_2,
+        SOME_TYPE_3,
+        SOME_TYPE_4,;
 
         override fun getType(): Int = ordinal
     }
